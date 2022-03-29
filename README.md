03c3d0e4ce955ecdc0abe221af7250358b41dc3c
Michael O P <98827447+michaelott241@users.noreply.github.com>
2022-03-29 02:32:49 +0000

    Completed Lab 6

--- README.md
+++ README.md
@@ -8,18 +8,22 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+In this lab, we used to seq3np1 function to keep track of the count during part A, while asking the user to input the upper range value. Then, we used the the upper bound value as a parameter to construct a graph, depicting the maximum so far. 
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 0
 
-Grace days remaining: < # >/5
+Grace days remaining: 5/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+To my knowledge, all parts were completed. 
 
 ## REFERENCES:
- < List any outside resources used >
+https://matthew-brett.github.io/teaching/string_formatting.html
+
+Used to better understand the string format method. 
+
+Additionally, the python library and lab description were used to complete the assignment. 
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+None 
--- main.py
+++ main.py
@@ -1,18 +1,47 @@
-
+import turtle
 def seq3np1(n):
     """
         Print the 3n+1 sequence from n, terminating when it reaches 1.
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count=0
     while(n != 1):
-        print(n)
+        count=count+1
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
-
+                      # the last print is 1
+    return count
+def drawLineGraph(upperBound):
+  window=turtle.Screen()
+  window.setworldcoordinates(0,0,10,10)
+  turtleMaxData=turtle.Turtle()
+  turtleDraw=turtle.Turtle()
+  turtleDraw.speed(0)
+  turtleDraw.up()
+  turtleMaxData.up()
+  turtleDraw.goto(0,0)
+  turtleDraw.down()
+  max_so_far=0
+  for i in range(1,upperBound):
+    result=seq3np1(i)
+    if(result>max_so_far):
+      max_so_far=result
+      turtleMaxData.clear()
+      turtleMaxData.goto(0,max_so_far)
+      str="Maximum so far: {}, {}".format(i,max_so_far)
+      turtleMaxData.write(str)
+    turtleDraw.goto(i,result)
+    window.setworldcoordinates(0,0,i+10,max_so_far+10)
+  window.exitonclick()
 def main():
-	seq3np1(3)
+  value = int(input("Enter an upper bound..."))
+  if(value<=0):
+    return
+  for i in range(1,value):
+    print(i)
+    print(seq3np1(i))
+  drawLineGraph(value)
 main()
